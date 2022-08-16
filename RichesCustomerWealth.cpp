a169abc9db6092c1862458dca6afabff2b85f9aa
melvinjariwala <melvinjariwala7.mj@gmail.com>
2022-08-16 12:56:06 +0530

    1672. Richest Customer Wealth

---
+++ RichesCustomerWealth.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int res=0;
+        for(auto customer:accounts){
+            res=max(res,accumulate(customer.begin(),customer.end(),0));
+        }
+        return res;
+    }
+};
